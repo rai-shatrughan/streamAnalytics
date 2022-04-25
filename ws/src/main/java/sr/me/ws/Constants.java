189a3e26d21b1c5de8a71f6043a0128c64fbc824
Shatrughan Rai <sr@sr.me>
2022-04-25 18:11:54 +0530

    prop->env

--- ws/src/main/java/sr/me/ws/Constants.java
+++ ws/src/main/java/sr/me/ws/Constants.java
@@ -2,10 +2,11 @@ package sr.me.ws;
 
 public class Constants {
 
-  public static final String KAFKA_BOOTSTRAP_SERVERS = System.getProperty("udv_kafkaBootstrapServers", "172.18.0.41:9092");
-  public static final String KAFKA_TS_TOPIC = System.getProperty("udv_kafkaTSTopic", "ts");
-  public static final String KAFKA_STREAM_TOPIC = System.getProperty("udv_kafkaStreamTopic", "stream");
+  public static final String KAFKA_BOOTSTRAP_SERVERS = System.getenv().getOrDefault("KAFKA_BOOTSTRAP_SERVERS", "172.18.0.41:9092");
+  public static final String KAFKA_TS_TOPIC = System.getenv().getOrDefault("KAFKA_TS_TOPIC", "ts");
+  public static final String KAFKA_STREAM_TOPIC = System.getenv().getOrDefault("KAFKA_STREAM_TOPIC", "stream");
 
-  public static final int WS_SERVER_PORT = Integer.parseInt(System.getProperty("udv_wsServerPort", "8000"));
+  public static final int WS_SERVER_PORT = Integer.parseInt(System.getenv().getOrDefault("WS_SERVER_PORT", "8000"));
 
+  public static final String POD_NAME = System.getenv().getOrDefault("POD_NAME", "unknown");
 }
