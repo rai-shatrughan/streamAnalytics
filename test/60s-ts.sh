aaa394d5716f0b66bbb76968ff88bc42f0c2a281
sr <sr@me>
2022-05-09 16:18:42 +0530

    test script

---
+++ test/60s-ts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+start=`date`
+echo "Start Time - " $start
+
+s=`date -u +%S`
+v1=$(($s + 10))
+v2=$(($s + 50))
+
+json="[{\"timestamp\": \"`date -u +\"%Y-%m-%dT%H:%M:%SZ\"`\",\"property\": \"temperature\",\"unit\": \"celcius\",\"value\": $v1},{\"timestamp\": \"`date -u +\"%Y-%m-%dT%H:%M:%SZ\"`\",\"property\": \"pressure\",\"unit\": \"ton\",\"value\": $v2}]"
+
+go-wrk -c 8000 -d 60 -T 30000 -M PUT \
+    -H "Content-Type: application/json" \
+    -body "$json" \
+    http://172.18.0.21:8000/api/v1/ts 
+
+
+end=`date`
+echo "End Time - " $end
