30feb67170e6a7de3ce4177a6c2134d351baf824
sr <sr@me>
2022-05-13 14:16:34 +0530

    test ts

--- ws/src/main/java/sr/me/ws/Constants.java
+++ ws/src/main/java/sr/me/ws/Constants.java
@@ -6,6 +6,7 @@ public class Constants {
   public static final String KAFKA_TS_TOPIC = System.getenv().getOrDefault("KAFKA_TS_TOPIC", "ts");
   public static final String KAFKA_STREAM_TOPIC = System.getenv().getOrDefault("KAFKA_STREAM_TOPIC", "stream");
 
+  public static final String WS_SERVER_IP = System.getenv().getOrDefault("WS_SERVER_IP", "172.18.0.21");
   public static final int WS_SERVER_PORT = Integer.parseInt(System.getenv().getOrDefault("WS_SERVER_PORT", "8000"));
 
   public static final String POD_NAME = System.getenv().getOrDefault("POD_NAME", "unknown");
---
+++ ws/src/test/java/sr/me/ws/TestVerticle.java
@@ -0,0 +1,63 @@
+package sr.me.ws;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.Future;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.HttpRequest;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.predicate.ResponsePredicate;
+import io.vertx.ext.web.codec.BodyCodec;
+import io.vertx.json.schema.common.dsl.Schemas;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.Instant;
+
+public class TestVerticle extends AbstractVerticle {
+  private HttpRequest<JsonObject> request;
+
+  public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+    vertx.deployVerticle(new TestVerticle());
+  }
+
+  @Override
+  public void start() {
+    request = WebClient.create(vertx) // (1)
+      .post(Constants.WS_SERVER_PORT, Constants.WS_SERVER_IP, "/api/v1/ts")
+      .ssl(false)  // (3)
+      .putHeader("Accept", "application/json")  // (4)
+      .as(BodyCodec.jsonObject()) // (5)
+      .expect(ResponsePredicate.SC_OK);  // (6)
+
+    vertx.setPeriodic(1, id -> sendTS());
+    // sendTS();
+  }
+
+  private void sendTS() {
+    request.sendJsonObject(buildTSJson())
+      .onSuccess(res -> {
+        System.out.print("Sent request" + res.statusCode() + ":" + res.statusMessage());
+        })
+      .onFailure(err -> {System.out.print("Failed request" + err.getCause());
+        });
+    }
+
+  private JsonObject buildTSJson(){
+    JsonObject json = new JsonObject()
+      .put("timestamp", Instant.now())
+      .put("property", "temperature")
+      .put("unit", "celcius")
+      .put("value", 100.01);
+
+    return json;
+  }
+
+}
