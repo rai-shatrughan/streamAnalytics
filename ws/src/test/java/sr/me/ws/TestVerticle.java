fd92ed0b328196ed208608b81602a3428697c4d5
sr <sr@me>
2022-05-16 17:29:09 +0530

    2k+

--- ws/src/test/java/sr/me/ws/TestVerticle.java
+++ ws/src/test/java/sr/me/ws/TestVerticle.java
@@ -2,37 +2,26 @@ package sr.me.ws;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
-import io.vertx.core.Future;
-import io.vertx.core.buffer.Buffer;
-import io.vertx.core.http.HttpMethod;
-import io.vertx.junit5.VertxExtension;
-import io.vertx.junit5.VertxTestContext;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
-import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.HttpRequest;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.predicate.ResponsePredicate;
 import io.vertx.ext.web.codec.BodyCodec;
-import io.vertx.json.schema.common.dsl.Schemas;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.time.Instant;
 
 public class TestVerticle extends AbstractVerticle {
+  private static final Logger logger = LogManager.getLogger();
   private HttpRequest<JsonObject> request;
 
   public static void main(String[] args) {
     int workers = 1000;
-    int countVertx = 1;
-    boolean enableWorker = true;
-
-    // VertxOptions vertxOptions = new VertxOptions();
-    // vertxOptions.setMaxEventLoopExecuteTime(10000);
-    // vertxOptions.setBlockedThreadCheckInterval(10000);
+    int countVertx = 2000;
+    boolean enableWorker = false;
     DeploymentOptions depOptions = new DeploymentOptions();
     depOptions.setWorker(true).setWorkerPoolSize(workers);
 
@@ -48,23 +37,23 @@ public class TestVerticle extends AbstractVerticle {
 
   @Override
   public void start() {
-    request = WebClient.create(vertx) // (1)
+    request = WebClient.create(vertx)
       .post(Constants.WS_SERVER_PORT, Constants.WS_SERVER_IP, "/api/v1/ts")
-      .ssl(false)  // (3)
-      .putHeader("Accept", "application/json")  // (4)
-      .as(BodyCodec.jsonObject()) // (5)
-      .expect(ResponsePredicate.SC_OK);  // (6)
+      .ssl(false)
+      .putHeader("Accept", "application/json")
+      .as(BodyCodec.jsonObject())
+      .expect(ResponsePredicate.SC_OK);
 
-    vertx.setPeriodic(1, id -> sendTS());
+    vertx.setPeriodic(1000, id -> sendTS());
     // sendTS();
   }
 
   private void sendTS() {
     request.sendJsonObject(buildTSJson())
       .onSuccess(res -> {
-        System.out.print("Sent request" + res.statusCode() + ":" + res.statusMessage());
+        logger.info("Sent request:" + res.statusCode() + ":" + res.statusMessage());
         })
-      .onFailure(err -> {System.out.print("Failed request" + err.getCause());
+      .onFailure(err -> {logger.error("Failed request:" + err.getCause());
         });
     }
 
