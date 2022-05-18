57da900471625e664fe013f8505a40d68fbd6cd7
sr <sr@me>
2022-05-18 19:15:40 +0530

    vertx workers and more

--- ws/src/test/java/sr/me/ws/TestVerticle.java
+++ ws/src/test/java/sr/me/ws/TestVerticle.java
@@ -19,22 +19,19 @@ public class TestVerticle extends AbstractVerticle {
   private HttpRequest<JsonObject> request;
 
   public static void main(String[] args) {
-    int workers = 1;
-    int countVertx = 4000;
     boolean enableWorker = true;
+    int workers = 2;
+    int countVertx = 3000;
+
     DeploymentOptions depOptions = new DeploymentOptions();
     depOptions.setWorker(true).setWorkerPoolSize(workers);
 
-    Vertx vertx = Vertx.vertx();
-    for(int i=0; i<=countVertx; i++){
-      try{
-      // ramp up period for all deployments
-      // 10 ms for each - 1 sec 100 deployments max - 5000 deply 50 sec
-        Thread.sleep(10);
-      } catch(Exception e){
-        e.printStackTrace();
-      }
+    VertxOptions vertxOptions = new VertxOptions();
+    // default event pool size is (no. of cores*2)
+    vertxOptions.setEventLoopPoolSize(1);
 
+    Vertx vertx = Vertx.vertx(vertxOptions);
+    for(int i=0; i<countVertx; i++){
       if (enableWorker) {
         vertx.deployVerticle(new TestVerticle(), depOptions);
       } else {
