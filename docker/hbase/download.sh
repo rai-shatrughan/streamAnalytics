78705a2b422d9b8f21b4510d7675acef3701f55c
sr <sr@debian-BULLSEYE-live-builder-AMD64>
2022-07-04 15:08:30 +0530

    hbase url

--- docker/hbase/download.sh
+++ docker/hbase/download.sh
@@ -7,5 +7,5 @@ if test -f "$FILE"; then
 else
     mkdir -p binary
     cd binary
-    wget https://dlcdn.apache.org/hbase/${HBASE_VERSION}/hbase-${HBASE_VERSION}-bin.tar.gz
+    wget https://archive.apache.org/dist/hbase/${HBASE_VERSION}/hbase-${HBASE_VERSION}-bin.tar.gz
 fi
