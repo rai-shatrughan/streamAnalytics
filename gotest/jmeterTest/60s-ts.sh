57307412e57a883de87b2eddc1595d5e3ab6e393
shatrughan <100255795+rai-shatrughan@users.noreply.github.com>
2022-07-06 16:32:44 +0530

    test

--- gotest/jmeterTest/60s-ts.sh
+++ gotest/jmeterTest/60s-ts.sh
@@ -5,7 +5,7 @@ echo "Start Time - " $start
 s=`date -u +%S`
 v1=$(($s + 10))
 
-json="{\"name\":\"sr_go_furn1\",\"timestamp\": \"`date -u +\"%Y-%m-%dT%H:%M:%SZ\"`\",\"property\": \"temperature\",\"unit\": \"celcius\",\"value\": $v1}"
+json="{\"name\":\"sr_go_furn1\",\"timestamp\": \"`date -u +\"%Y-%m-%dT%H:%M:%S.%sZ\"`\",\"property\": \"temperature\",\"unit\": \"celcius\",\"value\": $v1}"
 
 go-wrk -c 8000 -d 60 -T 30000 -M PUT \
     -H "Content-Type: application/json" \
