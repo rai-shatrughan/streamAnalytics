4fb7a72daf6cf26b2675ccac8c32b6179b4c2531
shatrughan <100255795+rai-shatrughan@users.noreply.github.com>
2022-07-06 14:56:24 +0530

    kc inp

--- ws/src/main/java/sr/me/ws/KCVerticle.java
+++ ws/src/main/java/sr/me/ws/KCVerticle.java
@@ -4,6 +4,12 @@ import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collector;
+import java.util.List;
+import java.util.ArrayList;
+
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
 
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.CqlSessionBuilder;
@@ -22,8 +28,10 @@ import io.vertx.core.Vertx;
 import io.vertx.core.tracing.TracingPolicy;
 import io.vertx.kafka.client.consumer.KafkaConsumer;
 import io.vertx.kafka.client.consumer.KafkaConsumerRecord;
+import io.vertx.core.json.JsonObject;
 
 import sr.me.common.Constants;
+import sr.me.handler.HbaseHandler;
 
 public class KCVerticle extends AbstractVerticle {
 
@@ -32,10 +40,21 @@ public class KCVerticle extends AbstractVerticle {
   static CassandraClient cassandraClient;
   Collector<Row, ?, String> listCollector;
 
+  static String TABLE_NAME = "myTable";
+  static String COLUMN_FAMILY = "cf1";
+  HbaseHandler hb;
+  Table table;
+
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
     initKafka(vertx);
-    initCassandra(vertx);
+
+    // initCassandra(vertx);
+
+    hb = new HbaseHandler();
+    hb.createHbaseTable(TABLE_NAME, COLUMN_FAMILY);
+    table = hb.getTable(TABLE_NAME);
+
     consumeWithPolling();
   }
 
@@ -103,19 +122,39 @@ public class KCVerticle extends AbstractVerticle {
 
       // Let's poll every second
       vertx.setPeriodic(500, timerId -> consumer.poll(Duration.ofMillis(100)).onSuccess(records -> {
+        List<Put> putList = new ArrayList<Put>();
         for (int i = 0; i < records.size(); i++) {
           KafkaConsumerRecord<String, String> record = records.recordAt(i);
           logger.info("Processing partition=" + record.partition() + " ,offset=" + record.offset());
           logger.debug("key=" + record.key() + ",value=" + record.value() + ",partition=" + record.partition()
               + ",offset=" + record.offset());
-          SimpleStatement simpleStatement = SimpleStatement.newInstance(
-              "INSERT INTO " + Constants.CASSANDRA_TABLE_TS + " JSON '" + record.value() + "'");
-          executeStatement(record.offset(), simpleStatement);
+
+          // SimpleStatement simpleStatement = SimpleStatement.newInstance(
+          //     "INSERT INTO " + Constants.CASSANDRA_TABLE_TS + " JSON '" + record.value() + "'");
+          // executeStatement(record.offset(), simpleStatement);
+          String value = record.value();
+          JsonObject jsonObject = new JsonObject(value);
+
+          String key = jsonObject.getString("name") + "_"
+              + jsonObject.getString("timestamp").replace(":", "-").replace(".", "-");
+          byte[] prow = Bytes.toBytes(key);
+
+          Put p = new Put(prow);
+          p.addColumn(COLUMN_FAMILY.getBytes(), key.getBytes(), value.getBytes());
+          putList.add(p);
+
         }
+        hb.puts(table, putList);
       }).onFailure(cause -> {
         logger.error("Something went wrong when polling " + cause.toString());
         // Stop polling if something went wrong
         vertx.cancelTimer(timerId);
+
+        try {
+          table.close();
+        } catch (Exception e){
+          logger.error(e.getCause());
+        }
       }));
     });
   }
